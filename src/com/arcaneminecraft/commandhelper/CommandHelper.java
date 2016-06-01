feab3a1490c745aca944e5808d4a150b6cc9ec1d
barestides <barestides@gmail.com>
2016-05-31 20:18:52 -0400

    initial commit

---
+++ src/com/arcaneminecraft/commandhelper/CommandHelper.java
@@ -0,0 +1,21 @@
+package com.arcaneminecraft.commandhelper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by barestides on 5/31/16.
+ */
+public class CommandHelper extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
+
+
